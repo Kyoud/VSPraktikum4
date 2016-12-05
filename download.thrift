aceb5257617a87cf78a9d8fbc5716c08b80c1d6a
debian <debian@debian.(none)>
2016-12-06 00:28:08 +0100

    text einlesen übertragen undspeichern und ausgeben fertig  Es fehlt: position speichern, ab bestimmter Position anfangen zu lesen

---
+++ download.thrift
@@ -0,0 +1,5 @@
+service download {
+	void ping(),
+	i32 download(1: string str, i32 length)
+
+}
---
+++ download_client.cpp
@@ -0,0 +1,52 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+#include <sys/time.h>
+#include <fstream>
+
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+
+#include "gen-cpp/download.h"// service
+
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace std::chrono;
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int n1 = 2, n2 = 3;
+    boost::shared_ptr<TSocket> socket(new TSocket("127.0.0.1", 9090));
+    boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    
+    downloadClient client(protocol);
+    try {
+	transport->open();
+	int length = 10;
+	
+	char b[10];
+	ifstream f("loreipsum.txt");
+
+	f.read(b, sizeof(b)); // Read one less that sizeof(b) to ensure null
+	cout << b;
+	string tmp(b);
+	std::cout <<"buffer: "<< b[0]<<"tmp: " << tmp;
+	int i = client.download(tmp, 0);
+	cout << "return: " <<i<<endl;
+	transport->close();
+	
+	
+    } catch (TException &tx) {
+        cout << "error calling server: " << tx.what() << endl;
+    }
+    return 0;
+}
+
